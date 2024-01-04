fb1133f995de22db18e38dfab2ccc43556129f71
“chid-hub” <chidhambaramcse@gmail.com>
2024-01-04 13:26:15 +0530

    Project created

---
+++ ArithmaticOperationTDD/Model/MathsModel.swift
@@ -0,0 +1,14 @@
+//
+//  MathsModel.swift
+//  ArithmaticOperationTDD
+//
+//  Created by LoaclUser on 04/01/24.
+//
+
+import Foundation
+
+class MathsModel{
+    
+   
+
+}
---
+++ ArithmaticOperationTDDTests/ArithmaticOperationTDDTests.swift
@@ -0,0 +1,15 @@
+//
+//  ArithmaticOperationTDDTests.swift
+//  ArithmaticOperationTDDTests
+//
+//  Created by LoaclUser on 04/01/24.
+//
+
+import XCTest
+@testable import ArithmaticOperationTDD
+
+final class ArithmaticOperationTDDTests: XCTestCase {
+    
+    
+    
+}
